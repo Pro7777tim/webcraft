0df6cd2c5dfa7a6a5e6e4d6d2c6d9ab6c340c5a8
Pro7777tim <khtim1226@gmail.com>
2024-11-07 17:30:08 +0200

    first commit

---
+++ core.js
@@ -0,0 +1,22 @@
+const game = {
+    world: {
+        name: name,
+        regime: regime,
+        complexity: complexity,
+        width: 100,
+        height: 400
+    },
+    player: {
+        health: 100,
+        hunger: 100,
+        air: 100,
+        warm: 100,
+        warm_dif: true,
+        power: 100,
+        emotion: 100,
+        x: 195,
+        y: 5,
+        speed_y: 0,//
+        speed_x: 0//
+    }
+}
